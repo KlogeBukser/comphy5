c4ef50ee5c8886aac0f3f7b08b4f560b629130d2
torstbje <torstbje@uio.no@users.noreply.github.com>
2022-12-12 17:21:02 +0100

    Included heat instruction and option for more than one time values

--- src/main.cpp
+++ src/main.cpp
@@ -32,40 +32,65 @@ int main(int argc, char* argv[])
 
     std::string instruction = argv[1];
     int n_slits = atoi(argv[2]);
-    double barrier_potential = 0.0;
-
-    // Takes the cmd_input for potential if there is a barrier in the middle. 
-    if (n_slits > 0) {
-        if (!eval_argc(argc,"barrier")) {
-            return ERR_MISSING_PARAMS;
-        }
-        barrier_potential = atof(argv[12]);
-    }
 
     double h = atof(argv[3]);
     double dt = atof(argv[4]);
-    double time = atof(argv[5]);
 
-    double xc = atof(argv[6]);
-    double sig_x = atof(argv[7]);
-    double px = atof(argv[8]);
+    double xc = atof(argv[5]);
+    double sig_x = atof(argv[6]);
+    double px = atof(argv[7]);
 
-    double yc = atof(argv[9]);
-    double sig_y = atof(argv[10]);
-    double py = atof(argv[11]);
-
+    double yc = atof(argv[8]);
+    double sig_y = atof(argv[9]);
+    double py = atof(argv[10]);
+    double barrier_potential = atof(argv[11]);
 
     if (instruction == "probability") {
+
+        double time = atof(argv[12]);
 
         // Makes filename
         string filename = make_filename(instruction, n_slits);
 
         // Makes the box
-        Box box = Box(n_slits,h, dt, xc, yc, px, py, sig_x, sig_y, barrier_potential);
+        Box box = Box(n_slits, h, dt, xc, sig_x, px, yc, sig_y, py, barrier_potential);
 
 
         // Evolve for the duration and writes total probability to file for each step
-        box.evolve_probability(time, filename);
+        box.write_probability(time, filename);
+
+    }
+
+    if (instruction == "heat") {
+
+        // Makes filename
+        string filename = make_filename(instruction, n_slits);
+
+        // Makes the box
+        Box box = Box(n_slits, h, dt, xc, sig_x, px, yc, sig_y, py, barrier_potential);
+
+        vec times(argc - 12);
+        for (int i = 0; i < argc - 12; i++) {
+            times[i] = atof(argv[i+12]);
+        }
+
+        // Evolve for the duration and writes total probability to file for each step
+        box.write_state(times, filename);
+
+    }
+
+    if (instruction == "evo") {
+
+        double time = atof(argv[12]);
+
+        // Makes filename
+        string filename = make_filename(instruction, n_slits);
+
+        // Makes the box
+        Box box = Box(n_slits, h, dt, xc, sig_x, px, yc, sig_y, py, barrier_potential);
+
+        // Evolve for the duration and writes total probability to file for each step
+        box.write_state(time, filename);
 
     }
 }
